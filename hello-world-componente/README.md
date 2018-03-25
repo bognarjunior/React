9610ec79bffd4080229bbe9799addd4f54fcebda
Bognar Junior <bognar_junior@yahoo.com.br>
2018-03-25 15:07:56 -0300

    Exemplo do blog
    React exemplo 2 do blog, criando o primeiro componente e utilizando state e props

---
+++ hello-world-componente/README.md
@@ -0,0 +1,4 @@
+Segundo exemplo de React no Blog. <br/>
+Criando um componente. <br/>
+
+Neste exemplo teremos um input onde o usuário poderá digitar um nome e aparecerá uma saudação na tela.
---
+++ hello-world-componente/src/App.js
@@ -0,0 +1,29 @@
+import React, {Component} from 'react';
+import Mensagem from './components/Mensagem';
+
+class App extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            message: 'Digite seu nome...'
+        }
+    }
+
+    handlerChange = (e) => {
+        console.log(e.target.value)
+        let message = (e.target.value === '') ? 'Digiter seu nome... ' : 'Olá,  ' + e.target.value; 
+        this.setState({...this.state, message: message });
+    }
+
+    render() {
+        return (
+            <div>
+                <input type="text" name="" id="" onChange={this.handlerChange}/>
+                <Mensagem  message={this.state.message}/>
+            </div>
+        );
+    }
+}
+
+export default App;
